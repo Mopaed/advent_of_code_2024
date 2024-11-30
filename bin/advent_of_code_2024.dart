101ae92f273a57c0bc802e1abc843534b3bdb380
mborzym <m.borzym@setlog.com>
2024-11-30 22:25:14 +0100

    :tada: Initial Commit

---
+++ bin/advent_of_code_2024.dart
@@ -0,0 +1,5 @@
+import 'package:advent_of_code_2024/advent_of_code_2024.dart' as advent_of_code_2024;
+
+void main(List<String> arguments) {
+  print('Hello world: ${advent_of_code_2024.calculate()}!');
+}
