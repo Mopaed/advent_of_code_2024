896475dc7fc2500b0ce71c834c22b5413b6c758b
mborzym <m.borzym@setlog.com>
2024-12-16 17:32:30 +0100

    add implementation of loading import

--- lib/day_two.dart
+++ lib/day_two.dart
@@ -1,3 +1,9 @@
+import 'package:advent_of_code_2024/file_reader.dart' as file_reader;
+
 List<List<int>> getInputList(String path) {
-  return List.empty();
+  return file_reader.getFileContent(path).map((e) {
+    return e.split(' ').map((i) {
+      return int.parse(i);
+    }).toList();
+  }).toList();
 }
