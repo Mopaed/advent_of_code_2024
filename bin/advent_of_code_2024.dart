57382825d744908279ffee0569790daddba63938
mborzym <m.borzym@setlog.com>
2024-12-17 18:04:58 +0100

    :sparkles: add solution to day_two part one

--- bin/advent_of_code_2024.dart
+++ bin/advent_of_code_2024.dart
@@ -1,6 +1,7 @@
 import 'package:advent_of_code_2024/advent_of_code_2024.dart'
     as advent_of_code_2024;
 import 'package:advent_of_code_2024/day_one.dart' as day_one;
+import 'package:advent_of_code_2024/day_two.dart' as day_two;
 
 void main(List<String> arguments) {
   print('Hello world: ${advent_of_code_2024.calculate()}!');
@@ -10,4 +11,7 @@ void main(List<String> arguments) {
       day_one
           .calculateSimilarityScore('./assets/files/day_one.txt')
           .toString());
+
+  print('Solution Day 02 Part 1: ' +
+      day_two.getNumberOfSaveReports('./assets/files/day_two.txt').toString());
 }
--- lib/day_two.dart
+++ lib/day_two.dart
@@ -9,5 +9,24 @@ List<List<int>> getInputList(String path) {
 }
 
 int getNumberOfSaveReports(String path) {
-  return 0;
+  int safeReports = 0;
+  List<List<int>> reports = getInputList(path);
+  reports.forEach((sublist) {
+    bool increase = sublist.elementAt(0) < sublist.elementAt(1);
+    bool safe = false;
+    for (int i = 0; i + 1 < sublist.length; i++) {
+      int diff = sublist[i] - sublist[i + 1];
+      if ((increase && diff < 0 && diff >= -3) ||
+          (!increase && diff > 0 && diff <= 3)) {
+        safe = true;
+      } else {
+        safe = false;
+        break;
+      }
+    }
+    if (safe) {
+      safeReports++;
+    }
+  });
+  return safeReports;
 }
